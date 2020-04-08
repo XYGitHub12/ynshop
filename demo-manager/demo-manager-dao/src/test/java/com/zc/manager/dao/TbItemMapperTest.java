9d70d2d130c3c17d1a4843acc68cb54c79f5f936
xueyu <1375156056@qq.com>
2020-04-08 15:49:48 +0800

    SSM分布式工程搭建并用逆向工程生成dao和po

---
+++ demo-manager/demo-manager-dao/src/test/java/com/zc/manager/dao/TbItemMapperTest.java
@@ -0,0 +1,21 @@
+package com.zc.manager.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.*;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration({"classpath:spring/spring-dao-test.xml"})
+//public class TbItemMapperTest {
+//
+//    @Autowired
+//    private TbItemMapper itemDao;
+//    @Test
+//    public void selectByPrimaryKey(){
+//        System.out.println(itemDao.selectByPrimaryKey(605616L).getTitle());
+//    }
+//}
