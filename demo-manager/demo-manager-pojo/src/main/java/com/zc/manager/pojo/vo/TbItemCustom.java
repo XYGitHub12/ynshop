a3f12836cbcb3bb49749d4eb3acb3d54547a075e
xueyu <1375156056@qq.com>
2020-04-10 10:11:37 +0800

    完成商品状态代码

--- demo-manager/demo-manager-pojo/src/main/java/com/zc/manager/pojo/vo/TbItemCustom.java
+++ demo-manager/demo-manager-pojo/src/main/java/com/zc/manager/pojo/vo/TbItemCustom.java
@@ -5,6 +5,15 @@ import com.zc.manager.pojo.po.TbItem;
 public class TbItemCustom extends TbItem {
 
     private String catName;
+    private String statusName;
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
 
     public String getCatName() {
         return catName;
